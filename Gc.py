34bf4a7d617cf4bfc0ccbaffde693e6f2235cad0
Aditidas-03 <149835867+Aditidas-03@users.noreply.github.com>
2023-11-10 14:23:46 +0100

    Update Gc.py

--- Gc.py
+++ Gc.py
@@ -2,7 +2,7 @@ from Bio import SeqIO
 max_gc = 0
 rosalind_id = None
 
-for seq_record in SeqIO.parse("/Users/aditi/Desktop/Rosalind1/rosalind_gc-2.txt", "fasta"):
+for seq_record in SeqIO.parse("/Users/aditi/Desktop/Rosalind1/GC/rosalind_gc-2.txt", "fasta"):
     seq=seq_record.seq
     gc_count = seq.count('G') + seq.count('C')
     total_bases = len(seq)
